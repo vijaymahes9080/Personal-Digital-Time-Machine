ab73d34d9539a125cbe3031bc8934e14cec8d2fe
Vijay Mahes <Vijaypradhap2004@gmail.com>
2026-07-21 20:16:58 +0530

    ci: use relative base path in vite config to support any GitHub Pages structure

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/Personal-Digital-Time-Machine/' : '/',
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
